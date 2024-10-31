afe6b3bf48a21b26815692a8b2d2fc115bce5e24
Lance Turbes <instinct_truster@protonmail.com>
2024-10-31 13:42:20 -0700

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import CalendarApp from "./components/CalendarApp";
+import "./components/calendar-app.css";
+
+function App() {
+  return (
+    <div className="container">
+      <CalendarApp />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/CalendarApp.jsx
@@ -0,0 +1,150 @@
+import { useState } from "react";
+
+const CalendarApp = () => {
+  const daysOfWeek = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+  const monthsOfYear = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
+
+  const currentDate = new Date();
+
+  const [currentMonth, setCurrentMonth] = useState(currentDate.getMonth());
+  const [currentYear, setCurrentYear] = useState(currentDate.getFullYear());
+  const daysInMonth = new Date(currentYear, currentMonth + 1, 0).getDate();
+  const firstDayOfMonth = new Date(currentYear, currentMonth, 1).getDay();
+
+  console.log(daysInMonth, firstDayOfMonth);
+
+  return (
+    <div className="calendar-app">
+      <div className="calendar">
+        <h1 className="heading">Calendar</h1>
+        <div className="navigate-date">
+          <h2 className="month">November,</h2>
+          <h2 className="year">2024</h2>
+          <div className="buttons">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="24px"
+              height="24px"
+              viewBox="0 0 24 24"
+            >
+              <path
+                fill="currentColor"
+                d="m15.914 17.5l-5.5-5.5l5.5-5.5L14.5 5.086L7.586 12l6.914 6.914z"
+              />
+            </svg>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="24px"
+              height="24px"
+              viewBox="0 0 24 24"
+            >
+              <path
+                fill="currentColor"
+                d="m8.086 17.5l5.5-5.5l-5.5-5.5L9.5 5.086L16.414 12L9.5 18.914z"
+              />
+            </svg>
+          </div>
+        </div>
+        <div className="weekdays">
+          {daysOfWeek.map((day) => (
+            <span key={day}>{day}</span>
+          ))}
+        </div>
+        <div className="days">
+          {[...Array(firstDayOfMonth).keys()].map((_, index) => (
+            <span key={`empty-${index}`} />
+          ))}
+          {[...Array(daysInMonth).keys()].map((day) => (
+            <span key={day + 1}>{day + 1}</span>
+          ))}
+        </div>
+      </div>
+      <div className="events">
+        <div className="event-popup">
+          <div className="time-input">
+            <div className="event-popup-time">Time</div>
+            <input
+              type="number"
+              name="hours"
+              min={0}
+              max={24}
+              className="hours"
+            />
+            <input
+              type="number"
+              name="minutes"
+              min={0}
+              max={60}
+              className="minutes"
+            />
+          </div>
+          <textarea placeholder="Enter Event Text (Maximum 60 Characters"></textarea>
+          <button className="event-popup-button">Add Event</button>
+          <button className="close-event-popup">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="24px"
+              height="24px"
+              viewBox="0 0 24 24"
+            >
+              <path
+                fill="currentColor"
+                d="m7.05 5.636l4.95 4.95l4.95-4.95l1.414 1.414l-4.95 4.95l4.95 4.95l-1.415 1.414l-4.95-4.95l-4.949 4.95l-1.414-1.414l4.95-4.95l-4.95-4.95z"
+              />
+            </svg>
+          </button>
+        </div>
+        <div className="event">
+          <div className="event-date-wrapper">
+            <div className="event-date">November 5, 2024</div>
+            <div className="event-time">10:00</div>
+          </div>
+          <div className="event-text">Workout with Waylon and Lance</div>
+          <div className="event-buttons">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="24px"
+              height="24px"
+              viewBox="0 0 24 24"
+            >
+              <path
+                fill="currentColor"
+                d="M15.748 2.947a2 2 0 0 1 2.828 0l2.475 2.475a2 2 0 0 1 0 2.829L9.158 20.144l-6.38 1.076l1.077-6.38zm-.229 3.057l2.475 2.475l1.643-1.643l-2.475-2.474zm1.06 3.89l-2.474-2.475l-8.384 8.384l-.503 2.977l2.977-.502z"
+              />
+            </svg>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="24px"
+              height="24px"
+              viewBox="0 0 512 512"
+            >
+              <path
+                fill="none"
+                d="M296 64h-80a7.91 7.91 0 0 0-8 8v24h96V72a7.91 7.91 0 0 0-8-8"
+              />
+              <path
+                fill="currentColor"
+                d="M432 96h-96V72a40 40 0 0 0-40-40h-80a40 40 0 0 0-40 40v24H80a16 16 0 0 0 0 32h17l19 304.92c1.42 26.85 22 47.08 48 47.08h184c26.13 0 46.3-19.78 48-47l19-305h17a16 16 0 0 0 0-32M192.57 416H192a16 16 0 0 1-16-15.43l-8-224a16 16 0 1 1 32-1.14l8 224A16 16 0 0 1 192.57 416M272 400a16 16 0 0 1-32 0V176a16 16 0 0 1 32 0Zm32-304h-96V72a7.91 7.91 0 0 1 8-8h80a7.91 7.91 0 0 1 8 8Zm32 304.57A16 16 0 0 1 320 416h-.58A16 16 0 0 1 304 399.43l8-224a16 16 0 1 1 32 1.14Z"
+              />
+            </svg>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default CalendarApp;
